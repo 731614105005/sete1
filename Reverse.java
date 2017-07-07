6c3ea4745722528bcee9fb6c9afd40c638c276ad
731614105005 <bawadharani96@gmail.com>
2017-07-07 14:56:39 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+# sete1
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Reverse
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    String a="bawa";
+    char[] s=a.toCharArray();
+    for(int i=s.length-1;i>=0;--i)
+    {
+      System.out.println(""+s[i]);
+      }
+      }
+      }
+      
